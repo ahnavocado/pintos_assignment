2a66d84407bd6bdeb3401bda5f6399e719357eeb
ahn_linux_virtualmachine <ahn.avocado@gmail.com>
2024-05-05 07:41:32 -0700

    create thread function

--- projects/automated_warehouse/automated_warehouse.c
+++ projects/automated_warehouse/automated_warehouse.c
@@ -18,87 +18,158 @@ struct message_box* boxes_from_central_control_node;
 struct message_box* boxes_from_robots;
 
 struct robot* robots;
+int num_robot ;
+
+void printMessage(struct message msg) {
+    printf("Robot Status - ");
+    printf("Row: %d  ", msg.row);
+    printf("Column: %d  ", msg.col);
+    printf("Payload: %d  ", msg.current_payload);
+    printf("Req Payload: %d  ", msg.required_payload);
+    printf("Next Command: %d\n\n", msg.cmd);
+}
+void send_message_from_robot(int robot_id, struct message msg) {
+    //struct message_box* box = &boxes_from_robots[robot_id];
+    printMessage(msg);
+    printf("67890 - %d\n", robot_id);
+    boxes_from_robots[robot_id].msg = msg;
+    if (!boxes_from_robots[robot_id].dirtyBit) {
+        boxes_from_robots[robot_id].dirtyBit = 1; // new message created
+    }
+    block_thread();
+
+}
+void send_message_from_central_control_node(int robot_id, struct message msg) {
+    //struct message_box* box = &boxes_from_central_control_node[robot_id];
+    
+    printMessage(msg);
+    printf("12345 - %d\n", robot_id);
+    if (!boxes_from_central_control_node[robot_id].dirtyBit) {
+        boxes_from_central_control_node[robot_id].msg = msg;
+        boxes_from_central_control_node[robot_id].dirtyBit = 1; // new message written
+    }
+    
+}
+struct message receive_message_from_robot(int robot_id) {
+    //struct message_box* box = &boxes_from_robots[robot_id];
+    while (!boxes_from_robots[robot_id].dirtyBit) {
+        // wait until new message
+        printf("NO message from robot -- %d \n ",robot_id);
+    }
+    //struct message msg = box->msg;
+    printMessage(boxes_from_robots[robot_id].msg);
+    boxes_from_robots[robot_id].dirtyBit = 0; // read msg
+    // for( int i = 0; i++; i < sizeof(boxes_from_robots) / sizeof(msg)){
+    //     struct message_box* box = &boxes_from_robots[i];
+    //     if(box -> dirtyBit == 1)
+    //     return msg;
+    // }
+    // unblock_threads();
+    return boxes_from_robots[robot_id].msg;
+}
+struct message receive_message_central_control_node(int robot_id) {
+    //struct message_box* box = &boxes_from_central_control_node[robot_id];
+    while (!boxes_from_central_control_node[robot_id].dirtyBit) {
+        // wait until new message
+        printf("NO message from central node -- %d\n",robot_id);
+        thread_sleep(100);
+    }
+    //struct message msg = box->msg;
+    printMessage(boxes_from_central_control_node[robot_id].msg);
+    boxes_from_central_control_node[robot_id].dirtyBit = 0; // read msg
+    return boxes_from_central_control_node[robot_id].msg;
+}
 
 // test code for central control node thread
 void test_cnt(){
         while(1){
-                print_map(robots, 4);
-                thread_sleep(1000);
+                print_map(robots, 5);
+                thread_sleep(10);
                 block_thread();
         }
-}
-
-void printMessage(struct message msg) {
-    printf("Robot Status:\t");
-    printf("Current Row: %d\t", msg.row);
-    printf("Current Column: %d\t", msg.col);
-    printf("Current Payload: %d\t", msg.current_payload);
-    printf("Required Payload: %d\t", msg.required_payload);
-    printf("Next Command: %d\n\n", msg.cmd);
 }
 
 // test code for robot thread
 void test_thread(void* aux){
         int idx = *((int *)aux);
         int test = 0;
-        // while(1){
-        //         printf("thread %d : %d\n", idx, test++);
-        //         thread_sleep(idx * 1000);
-        // }
+        while(1){
+                printf("thread %d : %d\n", idx, test++);
+                thread_sleep(idx * 1000);
+        }
 }
 
-void send_message_from_robot(int robot_id, struct message msg) {
-    struct message_box* box = &boxes_from_robots[robot_id];
-    printMessage(msg);
-    box->msg = msg;
-    if (!box->dirtyBit) {
-        box->dirtyBit = 1; // new message created
-    }
-    block_thread();
+// code for central control node thread
+void cnt_thread(){
+        int flag = 0;
+        while(1){
+            print_map(robots, 5);
+            for(int i = 1; i <= num_robot; i++){
+                printf("sending msg\n");
+                struct message msg = {0,0,0,0,12};
+                send_message_from_central_control_node(i,msg);
+            }
+            unblock_threads(); // unblock all threads
+
+            while(flag == 0){
+                for (int j = 1; j <= num_robot ; j++){
+                    if (boxes_from_robots[j].dirtyBit == 0){
+                        flag = 0;
+                        break;
+                    }
+                    flag = 1;
+                }
+                printf("waiting for reply\n");
+                thread_sleep(1000);
+            }
+            flag = 0;
+            for(int i = 1; i <= num_robot; i++){
+                printf("reading msg\n");
+                receive_message_from_robot(i);
+            }
+            print_map(robots, 5);
+            thread_sleep(1000);
+            // block_thread();
+        }
+        
+}
+
+
+// code for robot thread 물류 로봇은 중앙 관제 노드가 정해준 행동을 취한 후 자신의 상태 정보를 관제 노 드에게 전달 후 Block
+void robot_thread(void* aux){
+        int idx = *((int *)aux);
+        while(1){
+            struct message receive_msg = receive_message_central_control_node(idx);
+            
+            // struct robot* __robot = &robots[idx];
+            
+            if(receive_msg.cmd == 3){
+                robots[idx].col += 1;
+            }
+            else if(receive_msg.cmd == 6){
+                robots[idx].row += 1;
+            }
+            else if(receive_msg.cmd == 9){
+                robots[idx].col -= 1;
+            }
+            else if(receive_msg.cmd == 12){
+                robots[idx].row -= 1;
+            }
+            struct message send_msg =  {robots[idx].row,robots[idx].col,robots[idx].current_payload,robots[idx].required_payload};
+            send_message_from_robot(idx, send_msg);
+            //block_thread();
+        }
 
 }
-void send_message_from_central_control_node(int robot_id, struct message msg) {
-    struct message_box* box = &boxes_from_central_control_node[robot_id];
-    printMessage(msg);
-    if (!box->dirtyBit) {
-        box->msg = msg;
-        box->dirtyBit = 1; // new message written
-    }
-    
-}
-struct message receive_message_from_robot(int robot_id) {
-    struct message_box* box = &boxes_from_robots[robot_id];
-    while (!box->dirtyBit) {
-        // wait until new message
-        printf("NO message from robot");
-    }
-    struct message msg = box->msg;
-    printMessage(msg);
-    box->dirtyBit = 0; // read msg
-    for( int i = 0; i++; i < sizeof(boxes_from_robots) / sizeof(msg)){
-        struct message_box* box = &boxes_from_robots[i];
-        if(box -> dirtyBit == 1)
-        return msg;
-    }
-    unblock_threads();
-    return msg;
-}
-struct message receive_message_central_control_node(int robot_id) {
-    struct message_box* box = &boxes_from_central_control_node[robot_id];
-    while (!box->dirtyBit) {
-        // wait until new message
-        printf("NO message from central node");
-    }
-    struct message msg = box->msg;
-    printMessage(msg);
-    box->dirtyBit = 0; // read msg
-    return msg;
-}
+
+
 
 
 // entry point of simulator
 void run_automated_warehouse(char **argv)
 {       
+        //pintos/threads/build
+        //../../utils/pintos automated_warehouse 5 2A:4C:2B:2C:3A
         init_automated_warehouse(argv); // do not remove this
 
         printf("implement automated warehouse!\n");
@@ -107,6 +178,8 @@ void run_automated_warehouse(char **argv)
         char *ret_ptr;
         char *next_ptr;
         int robot_count = 1;
+        num_robot = sizeof(struct robot) * atoi(argv[1])/20;
+        
         
         // make robot
         robots = malloc(sizeof(struct robot) * atoi(argv[1])); // get number of robots
@@ -116,29 +189,22 @@ void run_automated_warehouse(char **argv)
         int *idxs = malloc(sizeof(int)* atoi(argv[1]));
         for (int i = 1; i < atoi(argv[1]) + 1; i++) // thread id index
                 idxs[i] = i;
-        threads[0] = thread_create("CNT", 0, &test_cnt, NULL); // creat main thread (idx == 0)
+        threads[0] = thread_create("CNT", 0, &cnt_thread, NULL); // creat main thread (idx == 0)
         
         // create thread for each robot
         ret_ptr = strtok_r(argv[2], ":", &next_ptr); //initialize token parsing
         while(ret_ptr) {
-                //printf("ret_ptr = [%s]\n", ret_ptr);
+                printf("ret_ptr = [%s]\n", ret_ptr);
+                
                 char name_buffer[4];
                 snprintf(name_buffer,4,"R%d",robot_count);
                 *idxs = robot_count;
                 setRobot(&robots[robot_count-1], name_buffer, 5, 5, ret_ptr[1], 0);
-                threads[robot_count] = thread_create(name_buffer, 0, &test_thread, &idxs[robot_count-1]); // thread idx starts from 1 
+                threads[robot_count] = thread_create(name_buffer, 0, &robot_thread, &idxs[robot_count-1]); // thread idx starts from 1 
                 ret_ptr = strtok_r(NULL, ":", &next_ptr);
                 robot_count++;
                 
         }
-        printf("dfsdfsf\n\n\n");
         
-        struct message myMessage = {3, 4, 10, 15, 1};
-
-        send_message_from_robot(1,myMessage);
- 
-       
-        //pintos/threads/build
-        //../../utils/pintos automated_warehouse 5 2A:4C:2B:2C:3A
         
 }
